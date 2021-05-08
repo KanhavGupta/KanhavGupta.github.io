13d3ec1877ec70763b0b3effd7382040519c5522
KanhavGupta <33150698+KanhavGupta@users.noreply.github.com>
2021-05-08 14:14:09 +0530

    Update contants.py

--- contants.py
+++ contants.py
@@ -4,8 +4,8 @@ CASC_PATH = './data/haarcascade_files/haarcascade_frontalface_default.xml'
 cascade_classifier = cv2.CascadeClassifier(CASC_PATH)
 EMOTIONS = ['angry', 'disgusted', 'fearful',
             'happy', 'sad', 'surprised', 'neutral']
-SET_MODE = 'test'
+# SET_MODE = 'test'
 ckpt_dir = './ckpt'
-train_data = './data/fer2013/fer2013.csv'
-valid_data = './my_images/'
+# train_data = './data/fer2013/fer2013.csv'
+# valid_data = './my_images/'
 show_box = True
