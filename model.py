6d41f24d96d3f90ffd4e44a7df37c860b2002654
KanhavGupta <33150698+KanhavGupta@users.noreply.github.com>
2021-05-08 14:12:02 +0530

    Major removals done

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 import demo
 import tensorflow as tf
-from utils import *
+# from utils import *
 from contants import *
 
 tfc = tf.compat.v1
@@ -64,37 +64,37 @@ def bias_variable(shape):
     return tfc.Variable(initial)
 
 
-def train_model(train_data):
-    fer2013 = input_data(train_data)
-    max_train_steps = 30001
+# def train_model(train_data):
+#     fer2013 = input_data(train_data)
+#     max_train_steps = 30001
 
-    x = tfc.placeholder(tf.float32, [None, 2304])
-    y_ = tfc.placeholder(tf.float32, [None, 7])
+#     x = tfc.placeholder(tf.float32, [None, 2304])
+#     y_ = tfc.placeholder(tf.float32, [None, 7])
 
-    y_conv = deepnn(x)
+#     y_conv = deepnn(x)
 
-    cross_entropy = tfc.reduce_mean(
-        tfc.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-    train_step = tfc.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tfc.equal(tfc.argmax(y_conv, 1), tf.argmax(y_, 1))
-    accuracy = tfc.reduce_mean(tfc.cast(correct_prediction, tf.float32))
-    with tfc.Session() as sess:
-        saver = tfc.train.Saver()
-        sess.run(tfc.global_variables_initializer())
-        for step in range(max_train_steps):
-            batch = fer2013.train.next_batch(8)
-            if step % 100 == 0:
-                train_accuracy = accuracy.eval(feed_dict={
-                    x: batch[0], y_: batch[1]})
-                print('step %d, training accuracy %g' % (step, train_accuracy))
-            train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+#     cross_entropy = tfc.reduce_mean(
+#         tfc.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+#     train_step = tfc.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+#     correct_prediction = tfc.equal(tfc.argmax(y_conv, 1), tf.argmax(y_, 1))
+#     accuracy = tfc.reduce_mean(tfc.cast(correct_prediction, tf.float32))
+#     with tfc.Session() as sess:
+#         saver = tfc.train.Saver()
+#         sess.run(tfc.global_variables_initializer())
+#         for step in range(max_train_steps):
+#             batch = fer2013.train.next_batch(8)
+#             if step % 100 == 0:
+#                 train_accuracy = accuracy.eval(feed_dict={
+#                     x: batch[0], y_: batch[1]})
+#                 print('step %d, training accuracy %g' % (step, train_accuracy))
+#             train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
-            if step + 1 == max_train_steps:
-                saver.save(sess, './models/emotion_model',
-                           global_step=step + 1)
-            if step % 1000 == 0:
-                print('*Test accuracy %g' % accuracy.eval(feed_dict={
-                    x: fer2013.validation.images, y_: fer2013.validation.labels}))
+#             if step + 1 == max_train_steps:
+#                 saver.save(sess, './models/emotion_model',
+#                            global_step=step + 1)
+#             if step % 1000 == 0:
+#                 print('*Test accuracy %g' % accuracy.eval(feed_dict={
+#                     x: fer2013.validation.images, y_: fer2013.validation.labels}))
 
 
 def image_to_tensor(image):
@@ -158,56 +158,56 @@ def test_one_file():
                     print("No Faces Detected")
 
 
-def valid_model(modelPath, validFile):
-    x = tfc.placeholder(tf.float32, [None, 2304])
-    y_conv = deepnn(x)
-    probs = tfc.nn.softmax(y_conv)
+# def valid_model(modelPath, validFile):
+#     x = tfc.placeholder(tf.float32, [None, 2304])
+#     y_conv = deepnn(x)
+#     probs = tfc.nn.softmax(y_conv)
 
-    saver = tfc.train.Saver()
-    ckpt = tf.train.get_checkpoint_state(modelPath)
-    with tfc.Session() as sess:
-        print(ckpt.model_checkpoint_path)
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            print('Restore model sucsses!!')
+#     saver = tfc.train.Saver()
+#     ckpt = tf.train.get_checkpoint_state(modelPath)
+#     with tfc.Session() as sess:
+#         print(ckpt.model_checkpoint_path)
+#         if ckpt and ckpt.model_checkpoint_path:
+#             saver.restore(sess, ckpt.model_checkpoint_path)
+#             print('Restore model sucsses!!')
 
-        files = os.listdir(validFile)
+#         files = os.listdir(validFile)
 
-        for file in files:
-            if file.endswith('.jpg'):
-                image_file = os.path.join(validFile, file)
-                image1 = cv2.imread(image_file, cv2.IMREAD_ANYCOLOR)
-                image = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
-                height, width = image.shape
-                if height*width == 2304:
-                    cv2.imwrite(f"detected/{file}", image)
-                    tensor = image_to_tensor(image)
-                    result = sess.run(probs, feed_dict={x: tensor})
-                    print(file, EMOTIONS[result.argmax()])
-                else:
-                    detected_faces, face_coors = demo.format_image(image)
-                    if detected_faces is not None:
-                        emotions = []
-                        for i, face in enumerate(detected_faces):
-                            cv2.imwrite(f"detected/{i}{file}", face)
-                            image_file = os.path.join("detected", str(i)+file)
-                            image = cv2.imread(
-                                image_file, cv2.IMREAD_GRAYSCALE)
-                            tensor = image_to_tensor(image)
-                            result = sess.run(probs, feed_dict={x: tensor})
-                            text = EMOTIONS[result.argmax()]
-                            emotions.append(text)
-                            print(image_file, text)
-                        for i, face_coor in enumerate(face_coors):
-                            [x1, y1, w1, h1] = face_coor
-                            cv2.rectangle(image1, (x1, y1),
-                                          (x1 + w1, y1 + h1), (255, 0, 0), 2)
-                            cv2.putText(
-                                image1, emotions[i], (x1, y1), cv2.FONT_HERSHEY_PLAIN, w1//110, (0, 255, 0), w1//130)
-                            cv2.imwrite(f"detected/orignal{file}", image1)
-                            try:
-                                os.remove(f"detected/{i}{file}")
-                            except:
-                                pass
-                    else:
-                        print("No Faces Detected")
+#         for file in files:
+#             if file.endswith('.jpg'):
+#                 image_file = os.path.join(validFile, file)
+#                 image1 = cv2.imread(image_file, cv2.IMREAD_ANYCOLOR)
+#                 image = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+#                 height, width = image.shape
+#                 if height*width == 2304:
+#                     cv2.imwrite(f"detected/{file}", image)
+#                     tensor = image_to_tensor(image)
+#                     result = sess.run(probs, feed_dict={x: tensor})
+#                     print(file, EMOTIONS[result.argmax()])
+#                 else:
+#                     detected_faces, face_coors = demo.format_image(image)
+#                     if detected_faces is not None:
+#                         emotions = []
+#                         for i, face in enumerate(detected_faces):
+#                             cv2.imwrite(f"detected/{i}{file}", face)
+#                             image_file = os.path.join("detected", str(i)+file)
+#                             image = cv2.imread(
+#                                 image_file, cv2.IMREAD_GRAYSCALE)
+#                             tensor = image_to_tensor(image)
+#                             result = sess.run(probs, feed_dict={x: tensor})
+#                             text = EMOTIONS[result.argmax()]
+#                             emotions.append(text)
+#                             print(image_file, text)
+#                         for i, face_coor in enumerate(face_coors):
+#                             [x1, y1, w1, h1] = face_coor
+#                             cv2.rectangle(image1, (x1, y1),
+#                                           (x1 + w1, y1 + h1), (255, 0, 0), 2)
+#                             cv2.putText(
+#                                 image1, emotions[i], (x1, y1), cv2.FONT_HERSHEY_PLAIN, w1//110, (0, 255, 0), w1//130)
+#                             cv2.imwrite(f"detected/orignal{file}", image1)
+#                             try:
+#                                 os.remove(f"detected/{i}{file}")
+#                             except:
+#                                 pass
+#                     else:
+#                         print("No Faces Detected")
